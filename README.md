2b355d82852a54409b0e2e22ad8fd73b28adab17
Tiger <xcao2005@gmail.com>
2026-01-23 09:37:27 -0500

    setup env and fastapi main entrypoint
    to define individual workers and shared memory & coordinator

--- README.md
+++ README.md
@@ -5,3 +5,7 @@ Let's lock in
 
 Essentially server for ingestion & processing
 FastAPI
+
+Use Python 3.11
+
+i'm gonna use venv instead of conda; feel free to use either though
--- main.py
+++ main.py
@@ -1,2 +1,43 @@
 #entry point
 
+import uvicorn
+from api.server import app
+import multiprocessing as mp
+
+import asyncio
+from fastapi import FastAPI
+from contextlib import asynccontextmanager
+
+# defines lifespan; handles startup and shutdown events
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    # Initialize resources here
+    # shared mem
+    #vision
+    #audio
+    #coordinator
+
+    yield #app running after this
+
+    # Cleanup resources here
+
+
+def start_server():
+    app=FastAPI(lifespan=lifespan)
+    return app
+
+    #initialize different workers
+    #initialize a coordinator
+
+
+
+app=start_server()
+if __name__ == "__main__":
+    #start subprocesses & then start server
+    try:
+        uvicorn.run("main:app", host="0.0.0.0", port=8000, log_level="error")
+    except KeyboardInterrupt:
+        print("Shutting down server...")
+        multiprocessing.current_process().terminate()
+    finally:
+        print("Server has been shut down.")
