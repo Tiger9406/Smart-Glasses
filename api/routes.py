2befaab71c58ff416867101e179cac8845b0b86a
Tiger <xcao2005@gmail.com>
2026-01-29 16:37:05 -0500

    remove redundant function

--- api/routes.py
+++ api/routes.py
@@ -10,10 +10,6 @@ router = APIRouter()
 #maybe we can make it so REST request to start connection & then websocket until REST close connection request
 #for now just do as if it's always websocketed
 
-def is_vision_data(data: bytes):
-     #logic for differentiating vision vs audio data        
-     #first byte is padded and for vision data == 1
-     return data[0] == 1
 
 @router.websocket("/stream")
 async def stream_ingest(websocket: WebSocket):
