43ec3f63136b0ef51e6eb86d0964321379adfb97
Tiger <xcao2005@gmail.com>
2026-02-04 15:02:28 -0500

    begin logic parsing raw detection faces; todo: deal with given scores & names

--- workers/vision.py
+++ workers/vision.py
@@ -4,15 +4,20 @@ import cv2
 import numpy as np
 
 from workers.base import BaseWorker
+from workers.vision_utils.facial_processing.tracking import FaceTracker
+from workers.vision_utils.facial_processing.inspireface_processor import InspireFaceProcessor
 
 
 class VisionWorker(BaseWorker):
     def setup(self, vision_command_queue: mp.Queue):
         # load whatever initial vision model u need; keep in mind single core so not gonna be performant
-        print("Vision Worker setting up")
+        print("[Vision] Worker setting up")
         self.command_queue = vision_command_queue
         self.objects_tracking = []
-        self.faces_tracking = []
+        self.tracker = FaceTracker(iou_threshold=0.3, untracked_before_delete=10)
+        self.processor = InspireFaceProcessor(model_path = "Megatron", confidence_threshold = 0.5, download_model = False)
+
+        print("[Vision] Ready")
 
     def _decode_image(self, raw_bytes):
         # turn byte sinto numpy array
@@ -36,10 +41,37 @@ class VisionWorker(BaseWorker):
             return
 
         frame = self._decode_image(self, raw_bytes)
-
         if frame is None:
             return
 
+        raw_detection_faces = self.processor.detect_faces(frame)
+        if not raw_detection_faces:
+            return 
+        
+        # use newly detected faces to update current holding
+        bboxes = [tuple(map(int, face.location)) for face in raw_detection_faces]
+        current_tracks = self.tracker.update(bboxes)
+
+        for i, track in enumerate(current_tracks):
+            raw_face_obj = raw_detection_faces[i]
+            should_identify = False
+            if track.identity == "Unkown":
+                should_identify = True
+
+            elif track.frames_since_recognition > 15:
+                should_identify = True
+
+            elif not track.is_confirmed:
+                should_identify = True
+
+            if should_identify:
+                embedding = self.processor.extract_embedding(frame, raw_face_obj)
+                name, score = self.processor.identify_embedding(embedding)
+
+                #TODO: deal with given score and names
+        
+        
+        
         commands = self._get_active_commands()
 
         # we have frame & commands; do basic stuff and depending on commands we'll do further processing
