b1e48e7779bbf63d545ae0f32979572c1f0e1278
Tiger <xcao2005@gmail.com>
2026-02-05 16:05:25 -0500

    improve data documentation

--- workers/coordinator.py
+++ workers/coordinator.py
@@ -38,14 +38,15 @@ class Coordinator(BaseWorker):
         event_type = event.get("type", "unknown")
 
         if event_type == "vision_result":
-            # given: list of the following:
-            # {
-            #     "track_id": track_id,
-            #     "bbox": (x, y, w, h),
-            #     "name": self.active_identities[track_id]["name"],
-            #     "score": self.active_identities[track_id]["score"],
-            #     "emb": emb
-            # }
+            # given list of the following:
+            """{
+                "track_id": track_id,
+                "bbox": (x, y, w, h),
+                "name": self.active_identities[track_id]["name"],
+                "score": self.active_identities[track_id]["score"],
+                "emb": emb
+            }"""
+
             faces = event.get("faces", [])
             if faces:
                 print(f"\n [Coordinator] Vision Event: detected {len(faces)} faces")
@@ -56,18 +57,18 @@ class Coordinator(BaseWorker):
                     _bbox = face.get("bbox")
                     # print(f" - ID: {face['track_id']} | Name: {name} ({score:.2f}) | Loc: {bbox}")
 
-        if event.get("type") == "speech":
-            print(f"[Coordinator] {event['text']}") 
-
-        # Current event structure
-        #                 
-        #     "type": "speech",
-        #     "text": text,    (would be the audio transcription)
-        #     "id": session_id,     
-        #     "timestamp": time.time(),
-        #     "final": False,
-        #     "name": Unkown,
-        #     "embedding": 
+        elif event_type == "speech":
+            #given:
+            """
+            "type": "speech",
+            "text": text,    (would be the audio transcription)
+            "id": session_id,
+            "timestamp": time.time(),
+            "final": False,
+            "name": Unkown,
+            "embedding":
+            """
+            print(f"[Coordinator] {event['text']}")
 
         else:
             print("\n[Coordinator] got other event")
