31755c12b3bd115b990327982aa47bb0301c2f9a
sheff2 <shaheff01@gmail.com>
2026-02-12 14:36:11 -0500

    updated config to have new audio

--- core/config.py
+++ core/config.py
@@ -1,4 +1,5 @@
 import os
+
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -13,17 +14,19 @@ HEADER_AUDIO = b"\x02"
 RESOLUTION = (1280, 720)
 FPS = 15
 FRAME_DELAY = 1.0 / FPS
-TARGET_VIDEO = './api/Friends_Clip.mp4'
+TARGET_VIDEO = "./api/Friends_Clip.mp4"
 
 # model for face rec
 DEFAULT_ISF_MODEL = os.getenv("DEFAULT_ISF_MODEL", "Megatron")
 MEGATRON_MODEL_PATH = os.getenv("MEGATRON_MODEL_PATH", "")
 PIKACHU_MODEL_PATH = os.getenv("PIKACHU_MODEL_PATH", "")
 
+
 def get_model_path(model_type):
     if model_type == "Megatron":
         return MEGATRON_MODEL_PATH
     return PIKACHU_MODEL_PATH
+
 
 CONFIDENCE_THRESHOLD_DETECTION = 0.5
 CONFIDENCE_THRESHOLD_MATCHING = 0.5
@@ -32,19 +35,22 @@ SAMPLE_RATE = 16000
 CHANNELS = 1
 SAMPLE_WIDTH = 2
 CHUNK_SIZE = 1024
-TARGET_AUDIO = "./api/OSR_us_11_16k.wav"
+TARGET_AUDIO = "./api/MattandShaun.wav"
 
 # model for audio
-PARAKEET_MODEL="mlx-community/parakeet-tdt-0.6b-v3"
+PARAKEET_MODEL = "mlx-community/parakeet-tdt-0.6b-v3"
 
-#chunking
-AUDIO_CHUNK_SIZE_MS=400     #800 old
-AUDIO_SAMPLE_RATE_HZ=16000
+# chunking
+AUDIO_CHUNK_SIZE_MS = 400  # 800 old
+AUDIO_SAMPLE_RATE_HZ = 16000
 
-#Streaming context, defaults used in parakeet readme
-CONTEXT_LEFT=64        #256 default both
-CONTEXT_RIGHT=64
+# Streaming context, defaults used in parakeet readme
+CONTEXT_LEFT = 64  # 256 default both
+CONTEXT_RIGHT = 64
 
-#variables for checking if a person stopped talking
-SPEECH_CHUNK_SIZE=1 #each chunk is 0.8 seconds so 3 chunks means they stop speaking for 2.4 seconds to signify a sentence break
-LOUDNESS_THRESHOLD=0.01 #how quiet it needs to be to signify stop talking, can tune this when we get mic based on backround noise
+# variables for checking if a person stopped talking
+SPEECH_CHUNK_SIZE = 1  # each chunk is 0.8 seconds so 3 chunks means they stop speaking for 2.4 seconds to signify a sentence break
+LOUDNESS_THRESHOLD = 0.01  # how quiet it needs to be to signify stop talking, can tune this when we get mic based on backround noise
+
+# voice diartization
+SIMILARITY_THRESHOLD = 0.55
