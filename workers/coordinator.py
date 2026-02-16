c948b30f7e02b41aaa86e343c221c7c3b1a6c58b
Tiger <xcao2005@gmail.com>
2026-02-16 17:02:03 -0500

    Fine tune VLM functionality; now testable with _test_VLM function in coordinator

--- workers/coordinator.py
+++ workers/coordinator.py
@@ -3,6 +3,7 @@
 
 import multiprocessing as mp
 import queue
+import time
 
 from workers.base import BaseWorker
 
@@ -10,28 +11,49 @@ from workers.base import BaseWorker
 
 
 class Coordinator(BaseWorker):
-    def __init__(self, results_queue: mp.Queue):
+    def __init__(self, results_queue: mp.Queue, commands_queue: mp.Queue):
         super().__init__()
         self.results_queue = results_queue
+        self.commands_queue = commands_queue
         # self.audio_events
         # self.vision_events
 
         # maybe more; hold past actions taken by self maybe? or a state, like what's happening in the world rn?
         # again, decision making module given the initial processing by the workers
 
+    def setup(self):
+        self.start_time = time.time()
+        self.request_number = 0
+
     def run(self):
         print("[Coordinator] Started")
+        self.setup()
+
         try:
             while self.running.is_set():
                 try:
                     event = self.results_queue.get(timeout=0.1)
                     self._handle_event(event)
+                    self._test_VLM()
                 except queue.Empty:
                     continue
                 except KeyboardInterrupt:
                     break
         finally:
             print("[Coordinator] Shutting down")
+
+    def _test_VLM(self):
+        # comment return statement to test VLM funcitonality
+        return
+        if time.time() - self.start_time > 5 and self.request_number == 0:
+            command = {
+                "cmd": "GET_VIDEO_CONTEXT",
+                "prompt": "Summarize the video in a sentence",
+                "request_id": self.request_number,
+            }
+            self.request_number += 1
+            print("[Coordinator] Put command in vision queue")
+            self.commands_queue.put_nowait(command)
 
     def _handle_event(self, event):
         # handling events; gotta coordinate event data format
@@ -75,6 +97,15 @@ class Coordinator(BaseWorker):
             "embedding":
             """
             print(f"[Coordinator] {event['name']}: {event['text']}")
+
+        elif event_type == "vlm_result":
+            """"
+            "type": "vlm_result",
+            "request_id": request_id,
+            "text": response_text,
+            "timestamp": time.time(),
+            """
+            print(f"[Coordinator] Received VLM output: {event['text']}")
 
         else:
             print("\n[Coordinator] got other event")
